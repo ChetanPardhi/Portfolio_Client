e88d484a1cc44f48eec187a8c2aeb9ce60c45149
Ashish Tripathy <ashish.tripathy@accolite.com>
2023-08-09 17:15:01 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { useNavigate } from "react-router-dom";
+import Navbar from "./component/navbar/Navbar";
+import Hero from "./component/hero/Hero";
+import { Routes, Route } from 'react-router-dom';
+import Login from './component/auth/Login';
+import React, { useState ,useEffect} from "react";
 
-function App() {
+const App = () => {
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(()=>{
+    if(sessionStorage.getItem("email")!==null){
+      setIsAuthenticated(true);
+    }
+  },[])
   return (
-    <div>
-
-    </div>
+    <>
+        {isAuthenticated ? (
+          <div>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+          </Routes>
+          </div>
+        )
+        :(
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          )}
+    </>
   );
-}
+};
 
 export default App;
