47782785d1b7fec1d6d0ac900143f7ac19b36666
Chetan Pardhi <chetan.pardhi@accolite.com>
2023-08-09 18:03:02 +0530

    sessionStorage

--- src/App.js
+++ src/App.js
@@ -5,29 +5,31 @@ import Hero from "./component/hero/Hero";
 import { Routes, Route } from 'react-router-dom';
 import Login from './component/auth/Login';
 import React, { useState ,useEffect} from "react";
+import PrivateRoutes from './component/privateRouts';
+
 
 const App = () => {
   
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  useEffect(()=>{
-    if(sessionStorage.getItem("email")!==null){
-      setIsAuthenticated(true);
-    }
-  },[])
+
   return (
     <>
-        {isAuthenticated ? (
-          <div>
-          <Routes>
-            <Route path="/" element={<Hero />} />
+    <Routes>
+        
+    <Route
+              element={
+                <>
+                  
+                  <PrivateRoutes />
+                </>
+              }
+            >
+              <Route path="/" element={<Hero />} />
+          
+          
+          </Route>
+            
+          <Route path="/login" element={<Login />} />
           </Routes>
-          </div>
-        )
-        :(
-          <Routes>
-            <Route path="/login" element={<Login />} />
-          </Routes>
-          )}
     </>
   );
 };
